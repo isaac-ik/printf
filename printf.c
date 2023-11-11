bd4472127650beb2fd409be1d930c7095a381105
Chiagozie Eziagor <eziagorchy@gmail.com>
2023-11-11 19:10:48 +0000

    Debug

--- printf.c
+++ printf.c
@@ -44,3 +44,29 @@ int _printf(const char *format, ...)
 	va_end(args);
 	return (count);
 }
+
+/**
+  * _print_str - Function name
+  * @str: Pointer to string
+  * Return: Integar value
+  */
+
+int _print_str(char *str)
+{
+	int count = 0;
+	char null_char[] = "(null)";
+
+	if (str == NULL)
+	{
+		count += _print_str(null_char);
+		return (count);
+	}
+
+	while (str[count] != '\0')
+	{
+		_putchar(str[count]);
+		count++;
+	}
+
+	return (count);
+}
